a8e42f071d5b3942b2bdb2d183112b8703167ad4
danellecline <dcline@mbari.org>
2024-07-04 18:21:10 -0700

    feat: added sdcat exemplar load

---
+++ aidata/commands/load_exemplars.py
@@ -0,0 +1,70 @@
+# aidata, Apache-2.0 license
+# Filename: commands/load_exemplars.py
+# Description: Load image embedding vectors from a SDCAT formatted CSV exemplar file
+import click
+import redis
+
+from aidata import common_args
+from aidata.logger import create_logger_file, info, err
+from aidata.plugins.extractors.tap_sdcat_csv import extract_sdcat_csv
+from aidata.plugins.loaders.tator.common import init_yaml_config
+from aidata.predictors.process_vits import ProcessVITS
+
+
+@click.command("exemplars", help="Load exemplars from a SDCAT formatted CSV exemplar file into a REDIS server")
+@common_args.yaml_config
+@common_args.dry_run
+@click.option("--input", type=str, required=True, help="input CSV file with SDCAT formatted CSV exemplar file ")
+@click.option("--label", type=int, help="Class label for the exemplars")
+@click.option("--batch-size", type=int, default=32, help="Batch size for loading embeddings")
+@click.option("--label", type=str, help="Class label for the exemplars. This is used as the base class name for the "
+                                        "exemplar images, e.g. Otter_0, Otter_1, etc.")
+
+def load_exemplars(config: str, input: str, dry_run: bool, label: str, batch_size=32, reset: bool=False) -> int:
+    """Load embeddings from a directory with SDCAT formatted exemplar CSV files. Returns the number of exemplar image
+    embeddings loaded."""
+    create_logger_file("load_exemplars")
+    try:
+        # Load the configuration file
+        # Each project needs a separate redis server for exemplar embeddings - this
+        # is done through separate ports
+        config_dict = init_yaml_config(config)
+        redi_host = config_dict["redis"]["host"]
+        redi_port = config_dict["redis"]["port"]
+        info(f"Connecting to REDIS server at {redi_host}:{redi_port}")
+        r = redis.Redis(host=redi_host, port=redi_port)
+        vits_processor = ProcessVITS(r, reset=reset, batch_size=batch_size)
+
+        info(f"Loading exemplars from {input}")
+        df = extract_sdcat_csv(input)
+
+        if dry_run:
+            info("Dry run mode. No data will be loaded. Found {len(df)} exemplars")
+            return len(df)
+
+        info(f'Processing {len(df)} exemplars')
+        image_paths = df.image_path.unique()
+        # If image paths are relative, prepend the base path
+        if not Path(image_paths[0]).is_absolute():
+            base_path = Path(input).parent
+            image_paths = [base_path / p for p in image_paths]
+
+        # Class names are indexed, e.g. Otter_0, Otter_1, etc.
+        # Each class name corresponds to an exemplar image that represents a subcluster
+        class_names = [f"{label}_{i}" for i in range(len(image_paths))]
+        vits_processor.load(image_paths, class_names)
+
+    except Exception as e:
+        err(f"Error: {e}")
+        raise e
+
+
+if __name__ == "__main__":
+    import os
+    from pathlib import Path
+    # To run this script, uncomment all @click decorators above
+    os.environ["ENVIRONMENT"] = "TESTING"
+
+    test_path = Path(__file__).parent.parent.parent / "tests" / "data" / "uav" / "otterexemplars.csv"
+    yaml_path = Path(__file__).parent.parent.parent / "tests"/ "config" / "config_sony.yml"
+    load_exemplars(config=yaml_path.as_posix(), dry_run=False,  input=test_path.as_posix(), label="Otter", batch_size=32, reset=True)
---
+++ aidata/predictors/process_vits.py
@@ -0,0 +1,86 @@
+# aidata, Apache-2.0 license
+# Filename: predictors/process_vits.py
+# Description: Process images with Vision Transformer (ViT) model and store/search in Redis
+from pathlib import Path
+
+import numpy as np
+import redis
+import torch
+from PIL import Image
+from transformers import ViTImageProcessor, ViTModel
+from typing import List
+
+from aidata.logger import info
+from aidata.predictors.vector_similarity import VectorSimilarity
+
+
+class ProcessVITS:
+
+    MODEL_NAME = 'google/vit-base-patch16-224'
+    VECTOR_DIMENSIONS = 768
+
+    def __init__(self, r: redis.Redis, reset: bool = False, batch_size: int = 32):
+        self.r = r
+        self.batch_size = batch_size
+        self.vs = VectorSimilarity(r, vector_dimensions=self.VECTOR_DIMENSIONS, reset=reset)
+
+        # Load the model and processor
+        self.device = "cuda" if torch.cuda.is_available() else "cpu"
+
+        if self.device == "cuda":
+            torch.cuda.set_device(self.device)
+
+        self.model = ViTModel.from_pretrained(self.MODEL_NAME)
+        self.processor = ViTImageProcessor.from_pretrained(self.MODEL_NAME)
+
+    @property
+    def model_name(self) -> str:
+        return self.MODEL_NAME
+
+    def preprocess_images(self, image_paths: List[Path]):
+        images = [Image.open(image_path).convert("RGB") for image_path in image_paths]
+        inputs = self.processor(images=images, return_tensors="pt").to(self.device)
+        return inputs
+
+    def get_image_embeddings(self, inputs: torch.Tensor):
+        """get embeddings for a batch of images"""
+        with torch.no_grad():
+            embeddings = self.model(**inputs)
+        batch_embeddings = embeddings.last_hidden_state[:, 0, :].cpu().numpy()
+        return np.array(batch_embeddings)
+
+    def load(self, image_paths: List[Path], class_names: List[str]):
+        """Load and preprocess batch of images and add to the vector similarity index"""
+
+        unique_class_names = list(set(class_names))
+        info(f'Found {len(image_paths)} images to load')
+
+        for i in range(0, len(image_paths), self.batch_size):
+            batch = image_paths[i:i + self.batch_size]
+            images = self.preprocess_images(batch)
+            embeddings = self.get_image_embeddings(images)
+            for j, emb in enumerate(embeddings):
+                self.vs.add_vector(doc_id=class_names[i + j], vector=emb.tobytes(), tag=self.MODEL_NAME)
+
+        info(f'Finished processing {len(image_paths)} images for {unique_class_names}')
+
+    def predict(self, image_paths: List[Path], top_n: int = 1) -> List[List[str]]:
+        """Search using KNN for embeddings for a batch of images"""
+        predictions = []
+        scores = []
+
+        info(f'Found {len(image_paths)} images to load')
+        for i in range(0, len(image_paths), self.batch_size):
+            batch = image_paths[i:i + self.batch_size]
+            images = self.preprocess_images(batch)
+            embeddings = self.get_image_embeddings(images)
+            for j, emb in enumerate(embeddings):
+                r = self.vs.search_vector(emb.tobytes(), top_n=top_n)
+                # Only grab the top_n class names, not the scores or other metadata
+                r_class_cluster = [x["id"].split(":")[-1] for x in r]
+                r_class = [x.split("_")[0] for x in r_class_cluster]
+                predictions.append(r_class)
+                # Separate out the scores for each prediction - this is used later for voting
+                scores.append([x["score"] for x in r])
+
+        return predictions, scores
