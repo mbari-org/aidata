764748c8cb0495c706bb1d66af52f879a70a13a0
danellecline <dcline@mbari.org>
2024-08-29 16:36:50 -0700

    test: passing load of cfe with depth

--- aidata/plugins/extractors/tap_cfe_media.py
+++ aidata/plugins/extractors/tap_cfe_media.py
@@ -28,20 +28,21 @@ def extract_media(image_path: Path, max_images: Optional[int] = None) -> pd.Data
 
     # Create a dataframe to store the combined data in an image_path column in sorted order
     images_df = pd.DataFrame()
-    allowed_extensions = [".png", ".jpg", ".jpeg", ".JPEG", ".PNG"]
-    images_df["image_path"] = [str(file) for file in image_path.rglob("*") if file.suffix.lower() in allowed_extensions]
+    images_df["image_path"] = [f.as_posix() for f in image_path.rglob("*")]
     images_df.sort_values(by="image_path")
-    if max_images:
+    if 0 < max_images < len(images_df):
         images_df = images_df.iloc[:max_images]
 
     # 'CFE_ISIIS-010-2024-01-26 10-14-07.102_0835_8.3m.png'
     pattern = re.compile(r"CFE_(.*?)-(\d+)-(\d{4}-\d{2}-\d{2} \d{2}-\d{2}-\d{2}\.\d{3})_(\d{4})_(\d+\.\d+)m\.(png|jpg|jpeg|JPEG|PNG)")
 
+    index = 0
+    images_df = images_df.groupby("image_path").first().reset_index()
+
     # Grab any additional metadata from the image name,
     iso_datetime = {}
     instrument_type = {}
-    index = 0
-    images_df = images_df.groupby("image_path").first().reset_index()
+    depth = {}
     info(f"Found {len(images_df)} unique images")
     fps = 17
     for group, df in images_df.groupby("image_path"):
@@ -49,23 +50,22 @@ def extract_media(image_path: Path, max_images: Optional[int] = None) -> pd.Data
         info(image_name)
         matches = re.findall(pattern, image_name)
         if matches:
-            instrument, _, datetime_str, frame_num, depth, ext = matches[0]
+            instrument, _, datetime_str, frame_num, depth_str, ext = matches[0]
             datetime_str = datetime_str + "Z"
             dt = datetime.strptime(datetime_str, "%Y-%m-%d %H-%M-%S.%fZ")
             dt_utc = pytz.utc.localize(dt)
             iso_datetime[index] = dt_utc
             instrument_type[index] = instrument
+            depth[index] = float(depth_str)
             increment_mseconds = int(int(frame_num) * 1e6 / fps)
             iso_datetime[index] = iso_datetime[index] + pd.Timedelta(microseconds=increment_mseconds)
-            index += 1
 
     if len(instrument_type) == 0:
         raise ValueError("No instrument type found in CFE image names")
     if len(iso_datetime) == 0:
         raise ValueError("No iso datetime found in image names")
-
-    if max_images:
-        images_df = images_df.head(max_images)
+    if len(depth) == 0:
+        raise ValueError("No depth found in image names")
 
     images_df["instrument"] = instrument_type
     images_df["iso_datetime"] = iso_datetime
